0b020cf4f5d15ca3bf00a47c28811732a605ff18
Rhiannon Journey <rhiannonjourney@gmail.com>
2023-10-25 13:40:56 -0400

    Revert "Revert "Add indicator column files""
    This reverts commit 69be6e9b4fc8fe729a09929380e0c3c32ccc2eaa.

---
+++ src/Tables/Columns/IndicatorColumn.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UnexpectedJourney\FilamentToolbox\Tables\Columns;
+
+use Filament\Tables\Columns\Column;
+
+class IndicatorColumn extends Column
+{
+
+
+    
+    protected string $view = 'tables.columns.indicator-column';
+}
